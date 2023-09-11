5a61246efa618f43ac9768567d4187c8908abe85
Carlos Batista <59052959+cbatista8a@users.noreply.github.com>
2023-09-11 12:25:07 +0200

    External init function for run observer (#1)
    * Export observer and monkey blockers to an external init function
    
    * add automated build package
    
    * Update npm-publish.yml

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import './observer'
-import './monkey'
-export { unblock } from './unblock'
+import { monkey } from './monkey';
+import { observer } from './observer';
+export { unblock } from './unblock';
+
+export function init() {
+    // Starts the monitoring
+    observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true
+    });
+    monkey();
+}
