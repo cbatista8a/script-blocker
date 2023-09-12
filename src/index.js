54dea2c1e4a0670aa93fdb106bc7d7b8975d549d
Carlos Batista <59052959+cbatista8a@users.noreply.github.com>
2023-09-12 14:39:59 +0000

    [feat] - add storage for manage user preferences

--- src/index.js
+++ src/index.js
@@ -1,47 +1,63 @@
-import { monkey } from './monkey';
-import { observer } from './observer';
-export { unblock } from './unblock';
-import {willBeUnblocked} from './checks';
+import { monkey } from "./monkey";
+import { observer } from "./observer";
+export { unblock } from "./unblock";
+import { willBeUnblocked } from "./checks";
+import { scriptsArray, STATUS_BLOCKED, STATUS_UNBLOCKED, Script, STORAGE_NAME } from "./variables";
 
 export function init() {
-    // Starts the monitoring
-    observer.observe(document.documentElement, {
-        childList: true,
-        subtree: true
-    });
-    monkey();
+  // Starts the monitoring
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  });
+  monkey();
 }
 
 export function openCookiesDialog() {
-    let dialog = document.querySelector('#cookie_dialog');
-    if(!dialog.open){
-        dialog.showModal();
-    }
+  let dialog = document.querySelector("#cookie_dialog");
+  if (!dialog.open) {
+    dialog.showModal();
+  }
 }
 
 export function closeCookiesDialog() {
-    let dialog = document.querySelector('#cookie_dialog');
-    if(dialog.open){
-        dialog.close();
-    }
+  let dialog = document.querySelector("#cookie_dialog");
+  if (dialog.open) {
+    dialog.close();
+  }
 }
 
 //Todo implement state save for scripts on session storage
 export function initializeOptions() {
-    let target_element_container = document.querySelector('#cookie_content');
-    let scripts = document.querySelectorAll('script');
+  let target_element_container = document.querySelector("#cookie_content");
+  let scripts = document.querySelectorAll("script");
+  const storage = getAvailableStorage();
+  scriptsArray = JSON.parse(storage.getItem(STORAGE_NAME)) || [];
+  scripts.forEach((script, index) => {
+    let status = STATUS_BLOCKED;
+    let label = document.createElement("label");
+    let option = document.createElement("input");
+    option.type = "checkbox";
+    option.className = "cookie-option";
+    option.value = script.src || index + 1;
+    if (willBeUnblocked(script)) {
+      option.setAttribute("checked", "checked");
+      status = STATUS_UNBLOCKED;
+    }
+    label.appendChild(option);
+    label.append(index + 1); // TODO set user friendly name
+    target_element_container.appendChild(label);
 
-    scripts.forEach((script, index) => {
-        let label = document.createElement('label');
-        let option = document.createElement('input');
-        option.type = 'checkbox';
-        option.className = 'cookie-option';
-        option.value = script.src || index + 1;
-        if(willBeUnblocked(script)){
-            option.setAttribute('checked','checked');
-        }
-        label.appendChild(option);
-        label.append(index + 1);
-        target_element_container.appendChild(label);
-    });
+    scriptsArray[index] = new Script(option.value, index, status);
+    storage.setItem(STORAGE_NAME, JSON.stringify(scriptsArray));
+  });
+}
+
+export function getAvailableStorage() {
+  try {
+    return window.localStorage ? window.localStorage : window.sessionStorage;
+  } catch (e) {
+    alert("Browser Storage not available and we don\'t able to handle user preferences for cookies");
+  }
+  return null;
 }
--- src/variables.js
+++ src/variables.js
@@ -1,4 +1,7 @@
 export const TYPE_ATTRIBUTE = 'javascript/blocked'
+export const STATUS_BLOCKED = 1;
+export const STATUS_UNBLOCKED = 0;
+export const STORAGE_NAME = "scripts-config";
 
 export const patterns = {
     blacklist: window.YETT_BLACKLIST,
@@ -8,4 +11,18 @@ export const patterns = {
 // Backup list containing the original blacklisted script elements
 export const backupScripts = {
     blacklisted: []
+}
+
+export let scriptsArray = [];
+
+export class Script {
+    constructor(src, position, status) {
+        this.src = src;
+        this.position = position;
+        this.status = status;
+    }
+
+    isBlocked() {
+        return this.status === STATUS_BLOCKED;
+    }
 }
