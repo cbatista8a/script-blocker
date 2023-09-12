c7f9d8dbea95af873f5ca8f5c5fd6851285c25de
Carlos Batista <59052959+cbatista8a@users.noreply.github.com>
2023-09-12 15:35:02 +0000

    [refactor] - set hash generator as async function

--- src/hasher.js
+++ src/hasher.js
@@ -1,12 +1,9 @@
 export async function generateSHA256Hash(text) {
   const encoder = new TextEncoder();
   const data = encoder.encode(text);
-  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray
-    .map((byte) => byte.toString(16).padStart(2, "0"))
-    .join("");
-  return hashHex;
+  return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
 }
 
 export async function verifyHash(text, hash) {
