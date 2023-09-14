60a5a87be61e859b7e5f98a652a66f942a6f2a1a
Carlos Batista <59052959+cbatista8a@users.noreply.github.com>
2023-09-14 13:15:52 +0000

    [refactor] - rename from modal-dialog to modal-handler

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
-import {init,unblock} from './src/index';
-import { openCookiesDialog, closeCookiesDialog } from "./src/modal-dialog";
+import {init} from './src/index';
+import { openCookiesDialog, closeCookiesDialog } from "./src/modal-handler";
 import dialog_modal from './src/dialog.html?raw';
 import styles from './src/style.css';
 
