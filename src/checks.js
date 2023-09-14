98f15a426e54faa3884bf2eb4bfbf066c78fc4c5
Carlos Batista <59052959+cbatista8a@users.noreply.github.com>
2023-09-14 10:53:26 +0000

    [feat] - unifies blocking verification

--- src/checks.js
+++ src/checks.js
@@ -17,3 +17,29 @@ export const willBeUnblocked = function(script) {
         patterns.whitelist && patterns.whitelist.some(entry => entry.test(src))
     )
 }
+
+export const shouldBlockScript = async function(script) {
+    const src = script.getAttribute('src');
+    const type = script.getAttribute('type');
+  
+    // Verificar el estado del script en user_preferences en el almacenamiento
+    const user_preferences = JSON.parse(localStorage.getItem(STORAGE_NAME)) || [];
+    const scriptId = await generateSHA256Hash(script.outerHTML);
+  
+    const userScript = user_preferences.find(scriptObj => scriptObj.id === scriptId);
+  
+    if (userScript) {
+      return userScript.isBlocked(); // Bloquear o desbloquear según el estado guardado en user_preferences.
+    }
+  
+    if (patterns.whitelist && patterns.whitelist.some(pattern => pattern.test(src))) {
+      return false; // No se bloquea si la URL del script coincide con un patrón de la lista blanca.
+    }
+  
+    if ((type === TYPE_ATTRIBUTE) || patterns.blacklist && patterns.blacklist.some(pattern => pattern.test(src))) {
+      return true; // Se bloquea si la URL del script coincide con un patrón de la lista negra.
+    }
+  
+    return true; // Por defecto, se bloquea.
+  };
+  
