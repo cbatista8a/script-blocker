137c35b938d5be200341ecce3bcee076eb33641f
Carlos Batista <59052959+cbatista8a@users.noreply.github.com>
2023-09-15 15:42:27 +0000

    [refactor] - optimize observer logic

--- src/observer.js
+++ src/observer.js
@@ -1,24 +1,28 @@
 import { shouldBlockScript } from "./checks";
 import { blockScript } from "./manager";
 import { generateSHA256Hash } from "./hasher";
+import { STATUS_BLOCKED, STATUS_UNBLOCKED } from "./variables";
+import { addUiOption } from "./ui";
 
 async function handleAddedScript(node) {
   const script_id = await generateSHA256Hash(node.outerHTML);
-  node.setAttribute('id',script_id);
+  node.setAttribute("id", script_id);
+  const script_name = node.getAttribute('data-name') || '';
+  let status = STATUS_UNBLOCKED;
   if (shouldBlockScript(script_id, node)) {
     blockScript(script_id, node);
+    status = STATUS_BLOCKED;
   }
+  addUiOption(script_id, status, script_name);
 }
 
 export const observer = new MutationObserver(trackScripts);
 
 function trackScripts(mutations) {
   for (const mutation of mutations) {
-    if (mutation.type == 'childList') {
-      for (const node of mutation.addedNodes) {
-        if (node.nodeType === Node.ELEMENT_NODE && node.tagName === "SCRIPT") {
-          handleAddedScript(node);
-        }
+    for (let node of mutation.addedNodes) {
+      if (node.tagName === "SCRIPT") {
+        handleAddedScript(node);
       }
     }
   }
