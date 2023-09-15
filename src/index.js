4949caaf9be99cef8d5ccef79cc208848ceded19
Carlos Batista <59052959+cbatista8a@users.noreply.github.com>
2023-09-15 16:12:07 +0000

    [refactor] - remove old references

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { monkey } from "./monkey";
 import { observer } from "./observer";
-export { unblock } from "./manager";
 
 import { openCookiesDialog, closeCookiesDialog, createModal } from "./modal-handler";
 import styles from "./style.css";
@@ -12,12 +11,12 @@ export function init() {
 
   createModal();
 
-  monkey();
+  // monkey();
   // Starts the monitoring
-  observer.observe(document.documentElement, {
-    childList: true,
-    subtree: true,
-  });
+  // observer.observe(document.documentElement, {
+  //   childList: true,
+  //   subtree: true,
+  // });
 }
 
 
