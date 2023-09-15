fd046bef6e248091341c5d2f72b577f978c22729
Carlos Batista <59052959+cbatista8a@users.noreply.github.com>
2023-09-15 15:45:37 +0000

    [refactor] - improve code

--- src/monkey.js
+++ src/monkey.js
@@ -1,5 +1,6 @@
 import { TYPE_ATTRIBUTE } from "./variables";
 import { shouldBlockScript } from "./checks";
+import {generateSHA256Hash} from './hasher';
 
 export function monkey() {
   const createElementBackup = document.createElement;
@@ -12,18 +13,19 @@ export function monkey() {
   // Monkey patch the createElement method to prevent dynamic scripts from executing
   document.createElement = function (...args) {
     // If this is not a script tag, bypass
-    const scriptElt = createElementBackup.bind(document)(...args);
+    const node = createElementBackup.bind(document)(...args);
     if (args[0].toLowerCase() !== "script") {
-      return scriptElt;
+      return node;
     }
-
     // Define getters / setters to ensure that the script type is properly set
+    const script_id = generateSHA256Hash(node.outerHTML);
+    debugger;
     try {
-      Object.defineProperties(scriptElt, {
+      Object.defineProperties(node, {
         src: {
           ...originalDescriptors.src,
           set(value) {
-            if (shouldBlockScript(scriptElt)) {
+            if (shouldBlockScript(script_id, node)) {
               originalDescriptors.type.set.call(this, TYPE_ATTRIBUTE);
             }
             originalDescriptors.src.set.call(this, value);
@@ -33,14 +35,14 @@ export function monkey() {
           ...originalDescriptors.type,
           get() {
             const typeValue = originalDescriptors.type.get.call(this);
-            if (typeValue === TYPE_ATTRIBUTE || shouldBlockScript(this)) {
+            if (shouldBlockScript(script_id,this)) {
               // Prevent script execution.
               return null;
             }
             return typeValue;
           },
           set(value) {
-            const typeValue = shouldBlockScript(scriptElt)
+            const typeValue = shouldBlockScript(script_id, node)
               ? TYPE_ATTRIBUTE
               : value;
             originalDescriptors.type.set.call(this, typeValue);
@@ -49,20 +51,23 @@ export function monkey() {
       });
 
       // Monkey patch the setAttribute function so that the setter is called instead
-      scriptElt.setAttribute = function (name, value) {
-        if (name === "type" || name === "src") scriptElt[name] = value;
-        else
-          HTMLScriptElement.prototype.setAttribute.call(scriptElt, name, value);
+      node.setAttribute = function (name, value) {
+        if (name === "type" || name === "src"){
+          node[name] = value;
+        } 
+        else{
+          HTMLScriptElement.prototype.setAttribute.call(node, name, value);
+        } 
       };
     } catch (error) {
       // eslint-disable-next-line
       console.warn(
         "Yett: unable to prevent script execution for script src ",
-        scriptElt.src,
+        node.src,
         ".\n",
         'A likely cause would be because you are using a third-party browser extension that monkey patches the "document.createElement" function.'
       );
     }
-    return scriptElt;
+    return node;
   };
 }
